db99ef54b25d46991653dc968428d940141e9342
Brit Barrick <britbarrick@Brits-Air.attlocal.net>
2021-12-26 13:50:03 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className="App">
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    constructor() {
+        super();
+        console.log('123');
+    }
+
+    render() {
+        return (
+            <div>
+              <h1>User Item</h1>  
+            </div>
+        )
+    }
+}
+
+export default UserItem
